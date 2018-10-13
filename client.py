3f513d74a23da30348e8b840605be114a0aa4f90
konradgalewski <42443563+konradgalewski@users.noreply.github.com>
2018-10-13 15:18:24 +0200

    Add files via upload

--- client.py
+++ client.py
@@ -1,12 +1,21 @@
 import socket
 
-TCP_IP="127.0.0.0"
-TCP_PORT=5555
-BUFFER_SIZE=1024
-Message="Hell my, name is WHO?"
+TCP_IP = "127.0.0.1"
+TCP_PORT = 5555
 
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+message = "Hell my, name is WHO?"
+
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.connect((TCP_IP,TCP_PORT))
-s.send(Message)
-s.close
+
+while True:
+    message= input()
+    msg = bytes(message, 'utf-8')
+    s.send(msg)
+
+    if not message:
+        break
+    
+    
+s.close()
 print("just send")
--- server.py
+++ server.py
@@ -1,17 +1,20 @@
 import socket
 
-TCP_IP="127.0.0.0"
+TCP_IP="127.0.0.1"
 TCP_PORT=5555
 BUFFER_SIZE=1024
 
-s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 s.bind((TCP_IP,TCP_PORT))
-s.listen(1)
-
-con, adres = s.accept()
-print("conneted to adr", adres)
-while 1:
-    data=con.recv(BUFFER_SIZE)
-    if not data:break
+#s.listen(1)
+#con, adres = s.accept()
+#print("conneted to adr", adres)
+while True:
+    
+    data = s.recv(BUFFER_SIZE)
+    
+    if not data:
+        break
     print("received", data)
-con.close()
+    
+s.close()
