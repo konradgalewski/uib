4989acfdd9c0b1089b502cb1c5276529502aca23
konrad galewski <galewski.k@gmail.com>
2018-11-07 11:39:23 +0100

    changes

--- collector_exec.py
+++ collector_exec.py
@@ -6,13 +6,7 @@ import pprint
 import time
 
 
-port_RX = '5557'  # port PULL
-
-
 ######  ZMQ
-# Socket to talk to server
-
-
 context = zmq.Context()
 collector_RX = context.socket(zmq.PULL)
 
@@ -20,16 +14,7 @@ collector_RX.bind("tcp://127.0.0.1:5557")
 collector_data = {}
 #########
 
-for x in range (1000): # TODO: PEP8
-    RX_msg = collector_RX.recv_json() # TODO: use a better name, 
+while True:
+    RX_msg = collector_RX.recv_json()  
     print(RX_msg)
 
-    #if collector_data.get(RX_msg['ID']):
-     #   collector_data[collector['consumer']] += 1
-    #else:
-     #   collector_data[collector['consumer']] = 1
-    #if x==99: # TODO: PEP8
-     #   pprint.pprint(collector_data)
-    #print(collector_data)
-
-   # time.sleep(1)
--- consumer_exec.py
+++ consumer_exec.py
@@ -5,10 +5,6 @@ import json
 import random
 import time
 
-
-port_RX = '5556'  
-port_TX = '5557'  
-
 ######  ZMQ
 context = zmq.Context()
 consumer_RX = context.socket(zmq.PULL)
@@ -23,19 +19,12 @@ while True:
     work = consumer_RX.recv_json() 
     print("from producer ",work) 
     exec(work['function'])
-    print(f(work['data a'],work['data b']))
-    a,b=(work['data a'],work['data b'])
-    work.pop('data a')
-    work.pop('data b')
-    work['data']=f(a,b)
-    
-
    
+    dct = dict()
+    dct['ID'] = work['ID']
+    dct['result'] = f(work['data'])   
+    time.sleep(random.randrange(1, 5))
     
-    #data = work['num']
-    #result = {'consumer':c_ID,'num':data} # TODO: PEP8
-    #print(result)
-#    if data==0:
-    consumer_TX.send_json(work)
-    print('send to collector  ',work)
-    time.sleep(1)
+    consumer_TX.send_json(dct)
+    print('send to collector  ',dct)
+   
